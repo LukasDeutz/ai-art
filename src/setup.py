a46df30281da193bf55a37e15f34106148b26b89
Lukas <scld@leeds.ac.uk>
2022-04-17 19:20:24 +0100

    Add setup.py

---
+++ src/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='ai-art',
+    version='0.0.1',
+    description='',
+    author='Lukas Deutz',
+    url='git@github.com:LukasDeutz/ai-art.git',
+    py_modules=['callback', 'loss_functions', 'normalize', 'reconstruct_content', 'reconstruct_style', 'transfer_style'],
+    packages=['optimizers'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    install_requires=[
+        'numpy==1.19.5',
+        'matplotlib==3.3.3',
+        'pandas==1.1.4',
+        'tensorflow==2.4.1',
+        'jupyter==1.0.0',
+        'tqdm==4.54.0',
+        'scipy==1.6.0',
+        'scikit-image==0.17.2'
+    ],
+)

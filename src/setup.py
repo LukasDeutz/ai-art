03738879778995010337cf83dfe84283e2b9453f
Lukas <scld@leeds.ac.uk>
2022-04-18 12:39:59 +0100

    Comment out requirements

--- src/setup.py
+++ src/setup.py
@@ -9,15 +9,15 @@ setup(
     py_modules=['callback', 'loss_functions', 'normalize', 'reconstruct_content', 'reconstruct_style', 'transfer_style'],
     packages=['optimizers'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    install_requires=[
-        'numpy==1.19.5',
-        'matplotlib==3.3.3',
-        'pandas==1.1.4',
-        'tensorflow==2.4.1',
-        'jupyter==1.0.0',
-        'tqdm==4.54.0',
-        'scipy==1.6.0',
-        'scikit-image==0.17.2'
-    ],
+    tests_require=['pytest']
+    # install_requires=[
+    #     'numpy==1.19.5',
+    #     'matplotlib==3.3.3',
+    #     'pandas==1.1.4',
+    #     'tensorflow==2.4.1',
+    #     'jupyter==1.0.0',
+    #     'tqdm==4.54.0',
+    #     'scipy==1.6.0',
+    #     'scikit-image==0.17.2'
+    # ],
 )
